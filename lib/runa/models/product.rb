204cc5b50ddd421eb491652afbaeb1baab894f35
Luke Oglesby <luke.oglesby@mo.work>
2024-09-09 22:13:33 +0100

    feat: add more product attributes
    Make many more product attributes accessible at the top level, rather than require the user to unnest them from the @gift_card attribute.

--- lib/runa/models/product.rb
+++ lib/runa/models/product.rb
@@ -7,15 +7,45 @@ class Runa::Product < Runa::Response
   attr_accessor :product_code
 
   # response/success
-  attr_accessor :code, :name, :description, :currency, :availability,
-                :countries_redeemable_in,
+  attr_accessor :available_denominations,
+                :availability,
+                :card_image_url,
                 :categories,
-                :state,
+                :code,
+                :content_resources,
+                :countries_redeemable_in,
+                :currency,
+                :denomination_type,
+                :description_url,
+                :e_code_usage_type,
                 :gift_card,
-                :subscription
+                :name,
+                :minimum_value,
+                :maximum_value,
+                :redeem_instructions_url,
+                :state,
+                :nonsense,
+                :terms_and_conditions_url
 
   def initialize(params = {})
     super
+
+    unless gift_card.nil?
+      denominations = gift_card['denominations']
+      @denomination_type = denominations['type']
+      @available_denominations = denominations['available_list']
+      @minimum_value = denominations['minimum_value']
+      @maximum_value = denominations['maximum_value']
+      @card_image_url = gift_card['assets']['card_image_url']
+      @e_code_usage_type = gift_card['e_code_usage_type']
+
+      @content_resources = gift_card['content_resources']
+      @description_url = @content_resources['description_markdown_url']
+      @redeem_instructions_url = @content_resources['redemption_instructions_markdown_url']
+      @terms_and_conditions_url = @content_resources['terms_consumer_markdown_url']
+    end
+
+    self
   end
 
   def path
